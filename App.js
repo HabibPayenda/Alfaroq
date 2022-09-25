d8c9551c2c94a56b83dea313a1757d7d549cd69d
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-09-25 09:26:57 +0430

    Adds the drawer navigation

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+
 import ExpensesScreen from './src/screens/ExpensesScreen';
 import IncomeScreen from './src/screens/IncomeScreen';
-
 import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
+  const Drawer = createDrawerNavigator();
+
   return (
-    <ExpensesScreen />
+    <NavigationContainer>
+      <Drawer.Navigator defaultScreenOptions={IncomeScreen}>
+      <Drawer.Screen name="Income" component={IncomeScreen} />
+      <Drawer.Screen name="Expenses" component={ExpensesScreen} />
+    </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
   loginScreen: {
     display: 'flex',
     justifyContent: 'center',
+    
     alignItems: 'center',
     height: '100%'
    },  
