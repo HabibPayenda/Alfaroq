b79aff35fcf478b4d58c810b433dc2f531e9a858
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-09-26 09:48:06 +0430

    Adds style improvement to expenses search screen

--- src/screens/ExpensesSearchScreen.js
+++ src/screens/ExpensesSearchScreen.js
@@ -5,6 +5,7 @@ import { perHeight, perWidth } from '../functions/heigthWidth';
 import Alfarooq from '../functions/Alfarooq';
 import colors from '../functions/colors';
 import ExpenseCard from '../components/ExpensesCard';
+import Btn from '../components/Btn';
 
 export default function ExpensesSearchScreen() {
   const [name, setName] = useState('');
@@ -40,7 +41,7 @@ export default function ExpensesSearchScreen() {
     <View style={styles.container}>
       <Text style={styles.title}>د مصارفو لټون</Text>
       <TextInput placeholder='د مصرف تاریخ دننه کړئ!' keyboardType='numeric' style={styles.input} value={name} onChangeText={(text) => setName(text)} />
-      <Button title="لټون" onPress={() => searchIncome()} />
+      <Btn text="لټون" onClick={searchIncome} />
       <FlatList
         data={data}
         keyExtractor={(item) => item.discription}
@@ -61,18 +62,20 @@ const styles = StyleSheet.create({
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    padding: perHeight(10),
+    paddingVertical: 30,
     height: perHeight(80),
     width: perWidth(100),
+    backgroundColor: colors.lightGray
   },
   input: {
-    borderColor: colors.darkGray,
-    borderWidth: 1,
     width: perWidth(80),
     height: 40,
     padding: 10,
+    paddingHorizontal: 20,
     marginBottom: 10,
-    borderRadius: 5,
+    borderRadius: 30,
+    backgroundColor: colors.light,
+    elevation: 10
   },
   title: {
     fontWeight: 'bold',
