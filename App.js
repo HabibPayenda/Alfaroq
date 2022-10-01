84aa7a723dbe583c9c3a4d4079fcb02911c668e4
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-10-01 16:58:32 +0430

    Fixes the headers

--- App.js
+++ App.js
@@ -4,7 +4,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
 import ExpensesScreen from './src/screens/ExpensesScreen';
 import IncomeScreen from './src/screens/IncomeScreen';
 import LoginScreen from './src/screens/LoginScreen';
@@ -14,40 +13,48 @@ import IncomeSearchScreen from './src/screens/IncomeSearchScreen';
 import ExpensesSearchScreen from './src/screens/ExpensesSearchScreen';
 import IncomeSearchDateScreen from './src/screens/IncomeSearchDateScreen';
 import ExpensesSearchDateScreen from './src/screens/ExpensesSearchDateScreen';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 export default function App() {
   const Drawer = createDrawerNavigator();
 
-const Stack = createStackNavigator();
+  const Stack = createStackNavigator();
 
-function IncomeStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="All Incomes" component={IncomeScreen} />
-      <Stack.Screen name="AddIncome" component={AddIncomeScreen} />
-      <Stack.Screen name="IncomeSearch" component={IncomeSearchScreen} />
-      <Stack.Screen name="IncomeSearchDate" component={IncomeSearchDateScreen} />
-    </Stack.Navigator>
-  );
-}
-function ExpenseStack() {
-  return (
-    <Stack.Navigator>
-      <Stack.Screen name="All Expenses" component={ExpensesScreen} />
-      <Stack.Screen name="Add Expense" component={AddExpenseScreen} />
-      <Stack.Screen name="ExpencesSearch" component={ExpensesSearchScreen} />
-      <Stack.Screen name="ExpencesSearchDate" component={ExpensesSearchDateScreen} />
-    </Stack.Navigator>
-  );
-}
-
+  function IncomeStack() {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="All Incomes" component={IncomeScreen} />
+        <Stack.Screen name="AddIncome" component={AddIncomeScreen} />
+        <Stack.Screen name="IncomeSearch" component={IncomeSearchScreen} />
+        <Stack.Screen name="IncomeSearchDate" component={IncomeSearchDateScreen} />
+      </Stack.Navigator>
+    );
+  }
+  function ExpenseStack() {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="All Expenses" component={ExpensesScreen} />
+        <Stack.Screen name="Add Expense" component={AddExpenseScreen} />
+        <Stack.Screen name="ExpencesSearch" component={ExpensesSearchScreen} />
+        <Stack.Screen name="ExpencesSearchDate" component={ExpensesSearchDateScreen} />
+      </Stack.Navigator>
+    );
+  }
 
   return (
     <NavigationContainer>
       <Drawer.Navigator defaultScreenOptions={IncomeStack}>
-      <Drawer.Screen name="Incomes" component={IncomeStack} />
-      <Drawer.Screen name="Expenses" component={ExpenseStack} />
-    </Drawer.Navigator>
+        <Drawer.Screen name="مرستې" component={IncomeStack} />
+        <Drawer.Screen name="مصارف" component={ExpenseStack} />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
