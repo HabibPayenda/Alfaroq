be328ef7c6b64445d9fcb663e6b56f155381605f
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-11-20 10:16:24 +0430

    Adds styles to menuItem

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
 import React from 'react'
 import colors from '../functions/colors'
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
 
 export default function MenuItem({navigation, path, name}) {
   return (
@@ -8,6 +10,12 @@ export default function MenuItem({navigation, path, name}) {
     onPress={() => navigation.navigate(path)}
     >
     <View style={styles.container}>
+    <MaterialCommunityIcons
+              style={{ color: colors.darkGray }}
+              name="database-search"
+              size={32}
+              color="black"
+            />
       <Text style={styles.text}> {name} </Text>
     </View>
     </TouchableWithoutFeedback>
@@ -17,14 +25,18 @@ export default function MenuItem({navigation, path, name}) {
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     width: '100%',
-    backgroundColor: colors.lightGray,
+    backgroundColor: colors.light,
     padding: 8,
-    marginBottom: 5
+    paddingHorizontal: 15,
+    marginBottom: 12,
+    elevation: 3
   },
   text: {
-    fontSize: 16
+    fontSize: 16,
+    fontWeight: 'bold'
   }
 })
