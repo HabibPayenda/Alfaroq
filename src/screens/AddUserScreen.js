d55a7c0799b7b858ca834e10d42b72d245492839
Habib <habiburahmanpayenda@gmail.com>
2023-03-11 16:38:48 +0430

    Adds basic modal layout in addUserScreen

--- src/screens/AddUserScreen.js
+++ src/screens/AddUserScreen.js
@@ -1,16 +1,19 @@
-import { StyleSheet, View, TextInput, ToastAndroid } from 'react-native';
+import { StyleSheet, View, TextInput, ToastAndroid, Modal, Text } from 'react-native';
 import React, { useState } from 'react';
 import { Picker } from '@react-native-picker/picker';
 
 import Btn from '../components/Btn';
 import colors from '../functions/colors';
-import { perWidth } from '../functions/heigthWidth';
+import { perHeight, perWidth } from '../functions/heigthWidth';
 import AlfarooqLogin from '../functions/AlfarooqLogin';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function AddUserScreen() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [isAdmin, setIsAdmin] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [userType, setUserType] = useState(3);
 
   function showToast() {
     ToastAndroid.show('صبر وکړئ!', ToastAndroid.SHORT);
@@ -56,10 +59,29 @@ export default function AddUserScreen() {
       />
       <TextInput
         style={styles.input}
-        value={password}
+        value={isAdmin}
         onChangeText={(text) => setPassword(text)}
         placeholder="پټه کلیمه"
       />
+      <View style={styles.selectUserTypeContainer}>
+        <Text>{userType === 2 ? 'کاروونکی' : 'کتونکی'}</Text>
+        <Btn onClick={() => setShowModal(true)} text="د کاروونکي بڼه وټاکې!" />
+      </View>
+
+      <Modal
+        animationType='slide'
+        visible={showModal}
+        onRequestClose={() => setShowModal(false)}
+        transparent={true}
+      >
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <Btn color={colors.light} textColor={colors.dark} width={90} text="کاروونکی" />
+            <Btn color={colors.light} textColor={colors.dark} width={90} text="کتونکی" />
+          </View>
+        </View>
+      </Modal>
+      
       <Picker
         style={styles.select}
         selectedValue={isAdmin}
@@ -96,6 +118,26 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     backgroundColor: colors.light,
     elevation: 10,
+  },
+  selectUserTypeContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: perWidth(70)
+  },
+  modal: {
+    width: perWidth(100),
+    height: perHeight(100),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalContent: {
+    backgroundColor: colors.darkGray,
+    padding: 15, 
+    paddingHorizontal: 40,
+    borderRadius: 5
   },
   select: {
     width: perWidth(80),
